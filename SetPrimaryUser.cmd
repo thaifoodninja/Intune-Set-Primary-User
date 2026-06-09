320617b660536b257f08415eb4ee3c0db5d9192c
thaifoodninja <114435182+thaifoodninja@users.noreply.github.com>
2026-06-09 13:30:12 -0400

    Update SetPrimaryUser.cmd

--- SetPrimaryUser.cmd
+++ SetPrimaryUser.cmd
@@ -1,17 +1,27 @@
 @echo off
 if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
+
 setlocal enabledelayedexpansion
 
 echo Retrieving Intune Device ID...
 
-for /f "tokens=2*" %%A in ('reg query "HKLM\SOFTWARE\Microsoft\Enrollments" /s /v DeviceID 2^>nul ^| findstr DeviceID') do (
-    set "DEVICE_ID=%%B"
+set "DEVICE_ID="
+set "BASE_KEY=HKLM\SOFTWARE\Microsoft\Enrollments"
+
+for /f "tokens=*" %%a in ('reg query "%BASE_KEY%" 2^>nul') do (
+
+    for /f "tokens=1,2,*" %%b in ('reg query "%%a\DMClient\MS DM Server" /v "EntDMID" 2^>nul ^| findstr "REG_SZ"') do (
+        set "DEVICE_ID=%%d"
+        set "FOUND_IN=%%a\DMClient\MS DM Server"
+    )
 )
+
 
 if defined DEVICE_ID (
     set "FULL_URL=https://intune.microsoft.com/#view/Microsoft_Intune_Devices/DeviceSettingsMenuBlade/~/properties/mdmDeviceId/!DEVICE_ID!"
     
-    echo Device ID: !DEVICE_ID!
+    echo Location  = !FOUND_IN!
+    echo Device ID = !DEVICE_ID!
     echo.
     echo Opening Intune portal...
     
